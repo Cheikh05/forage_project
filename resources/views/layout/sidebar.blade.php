f9fcdb49dd673346ef65633ef50ce8c2026adb85
Cheikh Kane <kane.cheikhtidiane95@gmail.com>
2019-05-20 18:06:15 +0000

    First Commit

---
+++ resources/views/layout/sidebar.blade.php
@@ -0,0 +1,87 @@
+<div class="sidebar" data-color="orange">
+  
+        <div class="logo">
+          <a href="{{ asset('http://www.creative-tim.com') }}" class="simple-text logo-mini">
+          
+          </a>
+          <a href="{{  asset('/dashbord') }}" class="simple-text logo-normal">
+            Sen Forage
+          </a>
+        </div>
+        <div class="sidebar-wrapper" id="sidebar-wrapper">
+          <ul class="nav">
+            <li class="active ">
+              <a href="{{  asset('/dashbord') }}">
+                <i class="now-ui-icons design_app"></i>
+                <p>Vue d'ensemble</p>
+              </a>
+            </li>
+          
+         
+            <li>
+                <a href="/laravelApp/public/gestionnaires">
+                  <i class="now-ui-icons users_single-02"></i>
+                  <p>Gestion des Gestionnaires</p>
+                </a>
+            </li>
+            <li>
+                <a href="./user.html">
+                  <i class="now-ui-icons users_single-02"></i>
+                  <p>Gestion des Comptes</p>
+                </a>
+            </li>
+            <li>
+                <a href="./user.html">
+                  <i class="now-ui-icons users_single-02"></i>
+                  <p>Gestion des Comptables</p>
+                </a>
+            </li>
+            <li>
+                <a href="./user.html">
+                  <i class="now-ui-icons users_single-02"></i>
+                  <p>Gestion des Controlleurs</p>
+                </a>
+            </li>
+          {{--   <li>
+              <a href="./map.html">
+                <i class="now-ui-icons location_map-big"></i>
+                <p>Maps</p>
+              </a>
+            </li> --}}
+           {{--  <li>
+                <a href="./tables.html">
+                  <i class="now-ui-icons design_bullet-list-67"></i>
+                  <p>Gestion des Villages</p>
+                </a>
+              </li>
+          
+                <li>
+                    <a href="./tables.html">
+                      <i class="now-ui-icons design_bullet-list-67"></i>
+                      <p>Gestion des Factures</p>
+                    </a>
+                  </li> --}}
+                  <li>
+                      <a href="./map.html">
+                        <i class="now-ui-icons location_map-big"></i>
+                        <p>Deconnexion</p>
+                      </a>
+                    </li>
+              
+            
+          
+         {{--    <li>
+              <a href="./typography.html">
+                <i class="now-ui-icons text_caps-small"></i>
+                <p>Typography</p>
+              </a>
+            </li> --}}
+            <li class="active-pro">
+              <a href="{{  asset('/dashbord') }}">
+                <i class="now-ui-icons arrows-1_cloud-download-93"></i>
+                <p>Page d'accueil</p>
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
---
+++ routes/web.php
@@ -0,0 +1,47 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+
+Route::get('/dashbord', function () {
+    return view("layout.default");
+});
+
+
+/* La route pour la gestion des Utilisateurs doivent etre préceder par le prfixe user */
+Route::prefix('user')->group(function () {
+        Route::get('/client', function () {
+            return view("layout.form_user",['client'=>'Client']);
+        });
+        Route::get('/gestionnaire', function () {
+            return view("layout.form_user",['client'=>'Gestionnaire']);
+        });
+        Route::get('/comptable', function () {
+            return view("layout.form_user",['client'=>'Comptable']);
+        });
+        Route::get('/controlleur', function () {
+            return view("layout.form_user",['client'=>'Controlleur']);
+        });
+        Route::get('/gestionnaires', function () {
+            return view("layout.gestionnaire.gestionnaires");
+        });     
+});
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+
+Route::resource('villages', 'VillageController');
