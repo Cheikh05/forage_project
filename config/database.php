3d92b6ef7fe9b3b1465c972cf0eb44c503705bcd
Cheikh Kane <kane.cheikhtidiane95@gmail.com>
2019-06-13 00:01:29 +0000

    Gestion de la migration

--- config/database.php
+++ config/database.php
@@ -44,7 +44,7 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', 'localhost'),
             'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'forge'),
             'username' => env('DB_USERNAME', 'forge'),
@@ -63,23 +63,23 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', 'ec2-23-21-91-183.compute-1.amazonaws.com'),
             'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', 'postgres://gxonqyhjdfteev:6290f85f8096e4095d4f563d5cea8ee57b513dd5ba0826407e919e5ff388c308@ec2-23-21-91-183.compute-1.amazonaws.com:5432/ddig9g6qs45u0e'),
+            'username' => env('DB_USERNAME', 'gxonqyhjdfteev'),
+            'password' => env('DB_PASSWORD', '6290f85f8096e4095d4f563d5cea8ee57b513dd5ba0826407e919e5ff388c30'),
             'charset' => 'utf8',
             'prefix' => '',
             'prefix_indexes' => true,
             'schema' => 'public',
-            'sslmode' => 'prefer',
+            'sslmode' => 'require',
         ],
 
         'sqlsrv' => [
             'driver' => 'sqlsrv',
             'host' => env('DB_HOST', 'localhost'),
             'port' => env('DB_PORT', '1433'),
-            'database' => env('DB_DATABASE', 'forge'),
+            'database' => env('DB_DATABASE', 'ddig9g6qs45u0e'),
             'username' => env('DB_USERNAME', 'forge'),
             'password' => env('DB_PASSWORD', ''),
             'charset' => 'utf8',
